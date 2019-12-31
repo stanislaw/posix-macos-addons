62ad3315f520e522ceaf8aa5a22ca3f67553b998
Stanislav Pankevich <s.pankevich@gmail.com>
2019-12-31 16:16:11 +0100

    minor cleanup

--- compatibility/compatibility.c
+++ compatibility/compatibility.c
@@ -1,28 +1,9 @@
 #include "compatibility.h"
 
 #include <signal.h>
-#include <string.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/syscall.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <assert.h>
 
 int sigqueue(pid_t pid, int sig, const union sigval value) {
-  printf("pid: %d, sig: %d, sigval:%d\n", pid, sig, value.sival_int);
+  /// sigqueue does not exit on macOS but it looks like it is enough if we
+  /// just send a signal with kill.
   return kill(pid, sig);
-
-//  siginfo_t *si;
-//  const size_t sz = sizeof(*si);
-//  si = calloc(1, sz);
-//  si->si_signo = SIGWINCH;
-//  si->si_code = SI_QUEUE;
-//  si->si_pid = getpid();
-//  si->si_uid = getuid();
-//  syscall(178, getpid(), SIGWINCH, si);
-//  free(si);
-//  fprintf(stderr, "Done.\n");
-//  return 0;
 }
