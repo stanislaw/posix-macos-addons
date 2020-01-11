6cd24032524ec5e9de6703f8d84f28db288fb19d
Stanislav Pankevich <s.pankevich@gmail.com>
2020-01-11 16:27:03 +0100

    pthread: pthread_setschedprio() boilerplate

---
+++ src/pthread/pthread-mac.c
@@ -0,0 +1,13 @@
+#include "pthread-mac.h"
+
+int pthread_setschedprio(pthread_t thread, int prio) {
+  /// TODO/Stanislaw: read the existing parameters from threads, not create new.
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+
+  struct sched_param parm;
+  parm.sched_priority = prio;
+  int result = pthread_attr_setschedparam(&attr, &parm);
+
+  return result;
+}
---
+++ src/pthread/pthread-mac.h
@@ -0,0 +1,11 @@
+#include <pthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int pthread_setschedprio(pthread_t thread, int prio);
+
+#ifdef __cplusplus
+}
+#endif
