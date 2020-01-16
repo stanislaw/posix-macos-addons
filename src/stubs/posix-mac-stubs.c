dd5ab9dfe0514ae1f49b20c8f2bd5bf886f4e392
Stanislav Pankevich <s.pankevich@gmail.com>
2020-01-16 21:22:44 +0100

    Add posix-mac-stubs with RT constants, useless but works

---
+++ src/stubs/posix-mac-stubs.c
@@ -0,0 +1,4 @@
+#include "posix-mac-stubs.c"
+
+const int SIGRTMIN = SIGUSR2 + 1;
+const int SIGRTMAX = 60;
---
+++ src/stubs/posix-mac-stubs.h
@@ -0,0 +1,4 @@
+#pragma once
+
+static const int SIGRTMIN = SIGUSR2 + 1;
+static const int SIGRTMAX = SIGUSR2 + 1;
