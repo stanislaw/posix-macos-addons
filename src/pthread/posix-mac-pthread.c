fab49fc93739f7b13f2578c99ef2c5dc48aac2cf
Stanislav Pankevich <s.pankevich@gmail.com>
2020-01-18 18:57:57 +0100

    pthread: proper pthread_setschedprio (closes #10)
    Ansible playbook for building on Ubuntu Linux

--- src/pthread/posix-mac-pthread.c
+++ src/pthread/posix-mac-pthread.c
@@ -1,13 +1,16 @@
 #include "posix-mac-pthread.h"
 
 int pthread_setschedprio(pthread_t thread, int prio) {
-  /// TODO/Stanislaw: read the existing parameters from threads, not create new.
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
+  struct sched_param param;
+  int current_policy;
 
-  struct sched_param parm;
-  parm.sched_priority = prio;
-  int result = pthread_attr_setschedparam(&attr, &parm);
+  int result = pthread_getschedparam(thread, &current_policy, &param);
+  if (result != 0) {
+    return result;
+  }
+
+  param.sched_priority = prio;
+  result = pthread_setschedparam(thread, current_policy, &param);
 
   return result;
 }
