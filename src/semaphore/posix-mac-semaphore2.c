5c6daeec22ccdb80b575a622f6c27684feda18a6
Stanislav Pankevich <s.pankevich@gmail.com>
2020-01-19 19:15:37 +0100

    semaphore: more safety checks for the sem2 implementation

--- src/semaphore/posix-mac-semaphore2.c
+++ src/semaphore/posix-mac-semaphore2.c
@@ -5,10 +5,9 @@
 #include <stdio.h>
 #include <time.h>
 
-static
-void timespec_diff(const struct timespec* lhs,
-                   const struct timespec* rhs,
-                   struct timespec* out) {
+static void timespec_diff(const struct timespec *lhs,
+                          const struct timespec *rhs,
+                          struct timespec *out) {
   assert(lhs->tv_sec <= rhs->tv_sec);
 
   out->tv_sec = rhs->tv_sec - lhs->tv_sec;
@@ -45,129 +44,174 @@ static dispatch_queue_t sync_queue() {
   static dispatch_once_t guard;
   static dispatch_queue_t queue;
   dispatch_once(&guard, ^{
-    queue = dispatch_queue_create("semaphore sync queue", DISPATCH_QUEUE_SERIAL);
+    queue =
+      dispatch_queue_create("semaphore sync queue", DISPATCH_QUEUE_CONCURRENT);
   });
   return queue;
 }
 
-static uint64_t MAGIC_NUMBER = UINT64_MAX - 0xdeadbeafU;
+static uint64_t SEMAPHORE_ALREADY_RELEASED = UINT64_MAX - 0xdeadbeafU;
 
-void reset_count(mac_sem2_t *psem) {
-  dispatch_sync(sync_queue(), ^{
-    dispatch_queue_set_specific(sync_queue(), *psem, (void *)0, NULL);
-  });
+static void reset_count(mac_sem2_t *psem) {
+  dispatch_queue_set_specific(sync_queue(), *psem, (void *)0, NULL);
 }
 
-void increment_count(mac_sem2_t *psem) {
-  dispatch_sync(sync_queue(), ^{
-    uint64_t
-      count = (uint64_t) dispatch_queue_get_specific(sync_queue(), *psem);
-    assert(count != (MAGIC_NUMBER - 1));
-    assert(count != MAGIC_NUMBER);
-    count++;
-    dispatch_queue_set_specific(sync_queue(), *psem, (void *) count, NULL);
-  });
+static void increment_count(mac_sem2_t *psem) {
+  uint64_t count = (uint64_t)dispatch_queue_get_specific(sync_queue(), *psem);
+  assert(count != (SEMAPHORE_ALREADY_RELEASED - 1));
+  assert(count != SEMAPHORE_ALREADY_RELEASED);
+  count++;
+  dispatch_queue_set_specific(sync_queue(), *psem, (void *)count, NULL);
 }
 
-void decrement_count(mac_sem2_t *psem) {
-  dispatch_sync(sync_queue(), ^{
-    uint64_t
-      count = (uint64_t) dispatch_queue_get_specific(sync_queue(), *psem);
-    assert(count != MAGIC_NUMBER);
-    count = count == 0 ? 0 : count - 1;
-    dispatch_queue_set_specific(sync_queue(), *psem, (void *) count, NULL);
-  });
+static void decrement_count(mac_sem2_t *psem) {
+  uint64_t count = (uint64_t)dispatch_queue_get_specific(sync_queue(), *psem);
+  assert(count != SEMAPHORE_ALREADY_RELEASED);
+  count = count == 0 ? 0 : count - 1;
+  dispatch_queue_set_specific(sync_queue(), *psem, (void *)count, NULL);
 }
 
-void finalize_count(mac_sem2_t *psem) {
-  dispatch_sync(sync_queue(), ^{
-    uint64_t count = MAGIC_NUMBER;
-    dispatch_queue_set_specific(sync_queue(), *psem, (void *) count, NULL);
-  });
+static void finalize_count(mac_sem2_t *psem) {
+  dispatch_queue_set_specific(
+    sync_queue(), *psem, (void *)SEMAPHORE_ALREADY_RELEASED, NULL);
 }
 
-uint64_t get_count(mac_sem2_t *psem) {
-  __block uint64_t count;
-  dispatch_sync(sync_queue(), ^{
-    count = (uint64_t) dispatch_queue_get_specific(sync_queue(), *psem);
-  });
-  return count;
+static uint64_t get_count(mac_sem2_t *psem) {
+  return (uint64_t)dispatch_queue_get_specific(sync_queue(), *psem);
 }
 
 int mac_sem2_init(mac_sem2_t *psem, int flags, unsigned count) {
-  *psem = dispatch_semaphore_create(count);
-  reset_count(psem);
+  dispatch_sync(sync_queue(), ^{
+    *psem = dispatch_semaphore_create(count);
+    reset_count(psem);
+  });
+
   return 0;
 }
 
 int mac_sem2_destroy(mac_sem2_t *psem) {
-  uint64_t count = get_count(psem);
-  if (count == MAGIC_NUMBER) {
-    printf("warning: mac_sem2_destroy: the over-release is detected\n");
-    return 0;
-  } else if (count > 0) {
-    printf("warning: mac_sem2_destroy: the unbalanced calls to wait/post is detected with count == %u\n", (uint32_t)count);
-    for (int i = 0; i < count; i++) {
-      dispatch_semaphore_signal(*psem);
+  dispatch_barrier_sync(sync_queue(), ^{
+    uint64_t count = get_count(psem);
+    if (count == SEMAPHORE_ALREADY_RELEASED) {
+      printf("warning: mac_sem2_destroy: the over-release is detected\n");
+      return;
+    } else if (count > 0) {
+      printf("warning: mac_sem2_destroy: the unbalanced calls to wait/post is "
+             "detected with count == %u\n",
+             (uint32_t)count);
+      for (int i = 0; i < count; i++) {
+        dispatch_semaphore_signal(*psem);
+      }
     }
-  }
-  finalize_count(psem);
-  dispatch_release(*psem);
+    finalize_count(psem);
+    dispatch_release(*psem);
+  });
   return 0;
 }
 
 int mac_sem2_post(mac_sem2_t *psem) {
-  uint64_t count = get_count(psem);
-  if (count == MAGIC_NUMBER) {
-    printf("warning: mac_sem2_post: post on released semaphore is detected\n");
-    return 0;
-  }
-  decrement_count(psem);
-  dispatch_semaphore_signal(*psem);
+  dispatch_sync(sync_queue(), ^{
+    uint64_t count = get_count(psem);
+    if (count == SEMAPHORE_ALREADY_RELEASED) {
+      printf(
+        "warning: mac_sem2_post: post on a released semaphore is detected\n");
+      return;
+    }
+    decrement_count(psem);
+    dispatch_semaphore_signal(*psem);
+  });
   return 0;
 }
 
 int mac_sem2_trywait(mac_sem2_t *psem) {
-  increment_count(psem);
-  int result = dispatch_semaphore_wait(*psem, DISPATCH_TIME_NOW);
-  if (result != 0) {
-    decrement_count(psem);
-    errno = ETIMEDOUT;
-    return -1;
-  }
-  return 0;
+  __block int result;
+  dispatch_sync(sync_queue(), ^{
+    uint64_t count = get_count(psem);
+    if (count == SEMAPHORE_ALREADY_RELEASED) {
+      printf("warning: mac_sem2_trywait: wait on a released semaphore is "
+             "detected\n");
+      result = 0;
+      return;
+    }
+    increment_count(psem);
+    result = dispatch_semaphore_wait(*psem, DISPATCH_TIME_NOW);
+    if (result != 0) {
+      decrement_count(psem);
+      errno = ETIMEDOUT;
+      result = -1;
+      return;
+    }
+    result = 0;
+  });
+  return result;
 }
 
 int mac_sem2_wait(mac_sem2_t *psem) {
-  uint64_t count = get_count(psem);
-  if (count == MAGIC_NUMBER) {
-    printf("warning: mac_sem2_wait: wait on released semaphore is detected\n");
-    return 0;
-  }
-  increment_count(psem);
-  dispatch_semaphore_wait(*psem, DISPATCH_TIME_FOREVER);
-  return 0;
+  __block int result;
+  dispatch_sync(sync_queue(), ^{
+    uint64_t count = get_count(psem);
+    if (count == SEMAPHORE_ALREADY_RELEASED) {
+      printf(
+        "warning: mac_sem2_wait: wait on a released semaphore is detected\n");
+      result = 0;
+      return;
+    }
+    increment_count(psem);
+  });
+
+  do {
+    dispatch_sync(sync_queue(), ^{
+      uint64_t count = get_count(psem);
+      if (count == SEMAPHORE_ALREADY_RELEASED) {
+        printf(
+          "warning: mac_sem2_wait: wait on a released semaphore is detected\n");
+        result = 0;
+        return;
+      }
+      result = dispatch_semaphore_wait(*psem, DISPATCH_TIME_NOW);
+      if (result != 0) {
+        decrement_count(psem);
+        errno = ETIMEDOUT;
+        result = -1;
+        return;
+      }
+    });
+  } while (result != 0);
+  return result;
 }
 
 int mac_sem2_timedwait(mac_sem2_t *psem, const struct timespec *abstim) {
-  increment_count(psem);
+  __block int result;
+  dispatch_sync(sync_queue(), ^{
+    uint64_t count = get_count(psem);
+    if (count == SEMAPHORE_ALREADY_RELEASED) {
+      printf("warning: mac_sem2_timedwait: wait on a released semaphore is "
+             "detected\n");
+      result = 0;
+      return;
+    }
+    increment_count(psem);
 
-  struct timespec now;
-  clock_gettime( CLOCK_REALTIME, &now);
+    struct timespec now;
+    clock_gettime(CLOCK_REALTIME, &now);
 
-  struct timespec diff;
-  timespec_diff(&now, abstim, &diff);
+    struct timespec diff;
+    timespec_diff(&now, abstim, &diff);
 
-  long diff_ns = diff.tv_sec * NSEC_PER_SEC + diff.tv_nsec;
-  dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, diff_ns);
+    long diff_ns = diff.tv_sec * NSEC_PER_SEC + diff.tv_nsec;
+    dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, diff_ns);
 
-  int result = dispatch_semaphore_wait(*psem, timeout);
-  if (result != 0) {
-    decrement_count(psem);
-    errno = ETIMEDOUT;
-    return -1;
-  }
-  return 0;
+    result = dispatch_semaphore_wait(*psem, timeout);
+    if (result != 0) {
+      decrement_count(psem);
+      errno = ETIMEDOUT;
+      result = -1;
+      return;
+    }
+    result = 0;
+    return;
+  });
+  return result;
 }
 
 int mac_sem2_getvalue(mac_sem2_t *sem, int *sval) {
