e11d10e02ee857f75b50259638e90c2c0cef3dd7
Stanislav Pankevich <s.pankevich@gmail.com>
2020-04-22 17:35:00 +0200

    semaphore: provide #defines to redirect normal sem_* symbols

--- src/semaphore/posix-mac-semaphore2-debug.h
+++ src/semaphore/posix-mac-semaphore2-debug.h
@@ -16,6 +16,18 @@ int mac_sem2_debug_wait(mac_sem2_t *psem);
 int mac_sem2_debug_timedwait(mac_sem2_t *psem, const struct timespec *abstim);
 int mac_sem2_debug_getvalue(mac_sem2_t *sem, int *sval);
 
+/// sem_* functions are available on macOS but they are deprecated.
+/// Additionally, the sem_timedwait() is not implemented on macOS.
+/// Redefining sem_* to mac_sem2_debug_*.
+#define sem_t mac_sem2_t
+#define sem_init mac_sem2_debug_init
+#define sem_destroy mac_sem2_debug_destroy
+#define sem_post mac_sem2_debug_post
+#define sem_wait mac_sem2_debug_wait
+#define sem_trywait mac_sem2_debug_trywait
+#define sem_timedwait mac_sem2_debug_timedwait
+#define sem_getvalue mac_sem2_debug_getvalue
+
 #ifdef __cplusplus
 }
 #endif
--- src/semaphore/posix-mac-semaphore2.h
+++ src/semaphore/posix-mac-semaphore2.h
@@ -16,6 +16,18 @@ int mac_sem2_wait(mac_sem2_t *psem);
 int mac_sem2_timedwait(mac_sem2_t *psem, const struct timespec *abstim);
 int mac_sem2_getvalue(mac_sem2_t *sem, int *sval);
 
+/// sem_* functions are available on macOS but they are deprecated.
+/// Additionally, the sem_timedwait() is not implemented on macOS.
+/// Redefining sem_* to mac_sem2_*.
+#define sem_t mac_sem2_t
+#define sem_init mac_sem2_init
+#define sem_destroy mac_sem2_destroy
+#define sem_post mac_sem2_post
+#define sem_wait mac_sem2_wait
+#define sem_trywait mac_sem2_trywait
+#define sem_timedwait mac_sem2_timedwait
+#define sem_getvalue mac_sem2_getvalue
+
 #ifdef __cplusplus
 }
 #endif
