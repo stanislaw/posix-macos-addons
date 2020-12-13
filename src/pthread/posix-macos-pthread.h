472907d2c1627a2bb3369d09e124ed327c3fd47f
Stanislav Pankevich <s.pankevich@gmail.com>
2020-12-13 13:25:55 +0100

    pthread: add pthread_mutex_timedlock

--- src/pthread/posix-macos-pthread.h
+++ src/pthread/posix-macos-pthread.h
@@ -5,6 +5,8 @@ extern "C" {
 #endif
 
 int pthread_setschedprio(pthread_t thread, int prio);
+int pthread_mutex_timedlock(pthread_mutex_t *mutex,
+                            const struct timespec *abs_timeout);
 
 #ifdef __cplusplus
 }
